5223bfe83b77ec320bc4e859b7ee8b0842e900e5
ozonenotes Kei Nomura <kei@ozonenotes.jp>
2015-11-15 20:59:43 +0900

    disable single post comments feed

--- mypace-remove-comments-feed-link.php
+++ mypace-remove-comments-feed-link.php
@@ -18,14 +18,19 @@ if ( $wp_version >= '4.4' ) {
 
 	if ( !function_exists( 'mypace_output_posts_feed' ) ){
 		remove_action( 'wp_head', 'feed_links' );
-
 		function mypace_output_posts_feed( ) {
 	?>
 	<link rel="alternate" type="<?php echo feed_content_type( 'rss2' ); ?>" title="<?php bloginfo( 'name' ); ?> &raquo; RSS 2.0 Feed" href="<?php bloginfo( 'rss2_url' ); ?>" />
 	<?php }
 		add_filter( 'wp_head','mypace_output_posts_feed' );
 	}
+}
 
+if ( !function_exists( 'mypace_remove_single_comments_feed' ) ){
+	function mypace_remove_single_comments_feed(){
+		return;
+	}
+	add_filter( 'post_comments_feed_link', 'mypace_remove_single_comments_feed' );
 }
 
 if( !function_exists( 'mypace_comments_feed_404' ) ){
